1984d455c00e8acb7bb33a96f207bf665d6e91ef
samjudson <sam@wackylabs.net>
2012-04-25 11:02:26 +0100

    Added Search page functionality

--- WindowsPhone7/Search.xaml.cs
+++ WindowsPhone7/Search.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using FlickrNet;
 
 namespace WindowsPhone7
 {
@@ -18,6 +19,35 @@ namespace WindowsPhone7
         public Search()
         {
             InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Flickr f = FlickrManager.GetAuthInstance();
+
+            PhotoSearchOptions options = new PhotoSearchOptions();
+            options.Tags = SearchTextBox.Text;
+            options.Extras = PhotoSearchExtras.LargeSquareUrl;
+
+            f.PhotosSearchAsync(options, r =>
+            {
+                if (r.Error != null)
+                {
+                    Dispatcher.BeginInvoke(() =>
+                    {
+                        MessageBox.Show("An error occurred talking to Flickr: " + r.Error.Message);
+                    });
+                    return;
+                }
+
+                PhotoCollection photos = r.Result;
+
+                Dispatcher.BeginInvoke(() =>
+                {
+                    ResultsListBox.ItemsSource = photos;
+                });
+            });
+
         }
     }
 }
