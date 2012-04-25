553fe1636527a6b52fa5ee58130e587393a24157
samjudson <sam@wackylabs.net>
2012-04-25 10:08:05 +0100

    Initial commit

---
+++ WindowsPhone7/FlickrManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using FlickrNet;
+
+namespace WindowsPhone7
+{
+    public class FlickrManager
+    {
+        public const string ApiKey = "3a68f22971d8d66b521b362c312c175c";
+        public const string SharedSecret = "b2acf0fb7910be24";
+
+        public static Flickr GetInstance()
+        {
+            return new Flickr(ApiKey, SharedSecret);
+        }
+    }
+}
