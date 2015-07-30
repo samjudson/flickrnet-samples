7439ae4ab7a5e05d9f6a4d9f3a1ea3077a621e18
samjudson <sam@wackylabs.net>
2015-07-30 15:44:43 +0100

    Added proof of concept Universal Windows App

---
+++ UniversalWindowsApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FlickrNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UniversalWindowsApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var f = new Flickr("3a68f22971d8d66b521b362c312c175c");
+
+            var photos = await f.PhotosSearchAsync(new PhotoSearchOptions { Tags = "colorful", PerPage = 500, SortOrder = PhotoSearchSortOrder.InterestingnessDescending });
+
+            PictureGrid.ItemsSource = photos;
+        }
+    }
+}
